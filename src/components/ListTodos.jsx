c64a05bc66d0c22b93bfd7290592d904bef94006
Artur-Flipdish <artur.mroczkowski@flipdish.ie>
2020-11-23 17:48:41 +0000

    Did use removeTodo straight from props

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -3,16 +3,12 @@ import Button from './Button';
 
 const ListTodos = ( { todos, removeTodo} ) => {
 
-    const deleteTodo = (todo) => {
-        removeTodo(todo);
-    }
-
     return(
       <ul>
         {todos.map((todo, index) => 
           <li key={index}>
             {todo}
-            <Button btnText='Remove' type='button' onClick={() => deleteTodo(todo)}/>    
+            <Button btnText='Remove' type='button' onClick={() => removeTodo(todo)}/>    
           </li>
         )}
       </ul> 
