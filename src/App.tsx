a34d3b3dde2b57c9a3df8a58c38ed62bdb5193d0
FlorinAndreas <florin@flipdish.ie>
2020-12-13 23:32:05 +0000

    Create Planning App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,4 @@
-import React from "react";
-import CreateTodo from "./components/CreateTodo";
-import ListTodos from "./components/ListTodos";
-
-import {createUseStyles} from 'react-jss';
-
-const useStyles = createUseStyles({
-  appBox: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: 'aqua',
-    padding: '20px',
-    paddingBottom: '20px',
-    margin: '0 auto',
-    width: '50%'
-  },
-  appName: {
-    color: 'red',
-    fontFamily: 'sans-serif',
-    textShadow: '2px 2px purple',
-    fontSize: '50px'
-  },
- 
-})
+import React from 'react';
 
 export type Todo = {
   id: number;
@@ -30,12 +6,9 @@ export type Todo = {
 };
 
 function App() {
-  const classes = useStyles();
   return (
-    <div className={classes.appBox}>
-      <h1 className={classes.appName}>Artur's Todo App</h1>
-      <CreateTodo />
-      <ListTodos />
+    <div>
+      <h1>Planning App</h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import reducer from "./reducers/todos.reducer";
-import { compose } from "redux";
-import thunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// const store = createStore(
-//   reducer,
-//   (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-//     (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+import App from './App';
+
+// const composeEnhancers =
+//   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
 // );
-
-const composeEnhancers =
-        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
