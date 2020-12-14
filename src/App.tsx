559da7d1133ee029bea51afd83d0ce7100abdc87
Rinat Sharifullin <74192788+flipdishRinat@users.noreply.github.com>
2020-12-14 16:53:35 +0000

    Bull is gone

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import SingleCard from "./components/Card";
 
 function App() {
   return (
     <div>
       <h1>Planning App</h1>
+      <SingleCard />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -25,7 +25,6 @@ const useStyles = makeStyles({
 
 export default function SingleCard() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>
