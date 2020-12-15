3636397151d98ce707879351b4eb7124ee4a8ada
rinatsharifullin <rinatsharifullin@flipdish.ie>
2020-12-15 12:30:42 +0000

    Wrap Fab in Box, Color props for Grid

--- src/components/PlanningContainer.tsx
+++ src/components/PlanningContainer.tsx
@@ -1,9 +1,7 @@
-import { Box, Container, Fab, Grid, Typography } from "@material-ui/core";
+import { Box, Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import AddEditDialog from "./AddEditDialog";
 import SingleCard from "./SingleCard";
-
-import ModalWindow from "./ModalWindow";
 
 export const PlanningContainer = () => {
   return (
@@ -20,28 +18,28 @@ export const PlanningContainer = () => {
         <AddEditDialog />
 
         <Grid container justify="center" spacing={2}>
-          <Grid item xs={4} justify="center">
+          <Grid item xs={4}>
             <Typography variant="h5">New</Typography>
             <Box mb={1}>
-              <SingleCard />
+              <SingleCard backColour="White" />
             </Box>
             <Box mb={1}>
-              <SingleCard />
+              <SingleCard backColour="White" />
             </Box>
             <Box mb={1}>
-              <SingleCard />
+              <SingleCard backColour="White" />
             </Box>
           </Grid>
           <Grid item xs={4}>
             <Typography variant="h5">In Progress</Typography>
             <Box mb={1}>
-              <SingleCard />
+              <SingleCard backColour="LightGreen" />
             </Box>
           </Grid>
           <Grid item xs={4}>
             <Typography variant="h5">Finished</Typography>
             <Box mb={1}>
-              <SingleCard />
+              <SingleCard backColour="LightSkyBlue" />
             </Box>
           </Grid>
         </Grid>
